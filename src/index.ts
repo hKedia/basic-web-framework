498ec943323673ed83a9ddd5955c47ce2bce088c
Harsh Kedia <harshkedia@outlook.in>
2020-03-24 11:55:55 +0100

    eventing system

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,14 @@ import { User } from "./models/User";
 
 const user = new User({ name: "test", age: 20 });
 
-console.log(user.get("name"));
+user.on("change", () => {
+  console.log("onChange");
+});
+user.on("change", () => {
+  console.log("onChange#2");
+});
+user.on("save", () => {
+  console.log("onSave");
+});
+
+user.trigger("change");
