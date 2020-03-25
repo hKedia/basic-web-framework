ef55cf03d57ca7c57a4afd6e26d18d5a940eed5c
Harsh Kedia <harshkedia@outlook.in>
2020-03-25 11:07:37 +0100

    generic class and inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "test", age: 20 });
+const user = User.buildUser({ id: 1 });
 
 user.on("change", () => {
-  console.log("onChange");
-});
-user.on("change", () => {
-  console.log("onChange#2");
-});
-user.on("save", () => {
-  console.log("onSave");
+  console.log(user);
 });
 
-user.trigger("change");
+user.fetch();
